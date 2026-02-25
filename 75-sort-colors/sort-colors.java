3b90725f2a0ec87989bf8772542033b294004c96
ganireddy-12 <itzganireddy12@gmail.com>
2026-02-25 17:21:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (42.31%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,16 +1,16 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int[] count=new int[3];
-        for(int num:nums)
-        {
-            count[num]++; //this array represents the count of 1,2,3 here how mnay times did they occur here
-        }
-        int index=0;
+         int[] arr=new int[3];
+         for(int n:nums)
+         {
+            arr[n]++; // there indeces are going to increment here staring array is in the form of 0000 
+         }
+         int k=0;
         for(int i=0;i<3;i++)
         {
-            for(int j=0;j<count[i];j++) //after these values are going to store here
+            for(int j=0;j<arr[i];j++)  //iterating up to how many 0's ,1's and 2's are there 
             {
-                nums[index++]=i;
+                nums[k++]=i;  // adding in that array
             }
         }
     }
